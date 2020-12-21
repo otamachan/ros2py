4482a3ea3b1ecdb8f5e5a8c0eb732aec9023e87d
Tamaki Nishino <otamachan@gmail.com>
2020-12-22 00:14:34 +0900

    Support MacOS

--- ros2py_init/ros2py_init/__init__.py
+++ ros2py_init/ros2py_init/__init__.py
@@ -1,8 +1,12 @@
 import argparse
 import pathlib
+import platform
 import sys
 
 SCRIPT_FILE = pathlib.Path(sys.prefix) / "bin" / "activate"
+VAR_NAME = "LD_LIBRARY_PATH"
+if platform.system() == "Darwin":
+    VAR_NAME = "DYLD_LIBRARY_PATH"
 
 
 def insert_after(content: str, match_line: str, new_line: str) -> str:
@@ -27,18 +31,18 @@ def update() -> None:
         print(f"{SCRIPT_FILE} does not exist")
         return
     script = SCRIPT_FILE.read_text()
-    if "LD_LIBRARY_PATH" in script:
+    if VAR_NAME in script:
         print("activate is alrady updated")
         return
     script = insert_after(
         script,
         "export PATH",
-        '_OLD_LD_LIBRARY_PATH="${LD_LIBRARY_PATH}"; LD_LIBRARY_PATH="$VIRTUAL_ENV/lib:$LD_LIBRARY_PATH"; export LD_LIBRARY_PATH; echo LD_LIBRARY_PATH=${LD_LIBRARY_PATH}',
+        f'_OLD_{VAR_NAME}="${{{VAR_NAME}}}"; {VAR_NAME}="$VIRTUAL_ENV/lib:${VAR_NAME}"; export {VAR_NAME}; echo {VAR_NAME}=${{{VAR_NAME}}}',
     )
     script = insert_after(
         script,
         "        unset _OLD_VIRTUAL_PATH",
-        '        LD_LIBRARY_PATH="${_OLD_LD_LIBRARY_PATH}"; export LD_LIBRARY_PATH; unset _OLD_LD_LIBRARY_PATH;',
+        f'        {VAR_NAME}="${{_OLD_{VAR_NAME}}}"; export {VAR_NAME}; unset _OLD_{VAR_NAME};',
     )
     SCRIPT_FILE.write_text(script)
     print("updated")
@@ -49,10 +53,10 @@ def restore() -> None:
         print(f"{SCRIPT_FILE} does not exist")
         return
     script = SCRIPT_FILE.read_text()
-    if "LD_LIBRARY_PATH" not in script:
+    if VAR_NAME not in script:
         print("nothing to do")
         return
-    script = remove_line_if_includes(script, "LD_LIBRARY_PATH")
+    script = remove_line_if_includes(script, VAR_NAME)
     SCRIPT_FILE.write_text(script + "\n")
     print("restored")
 
